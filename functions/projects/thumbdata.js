7781dd3ca8e2b1ebdd41014db4a84ed748f0d6f4
Andrew Newstead <>
2020-06-23 20:12:43 +0100

    change folder

--- functions/projects/thumbdata.js
+++ functions/projects/thumbdata.js
@@ -1,5 +1,5 @@
 const fs = require("fs");
-const data = fs.readFileSync(require.join(__dirname, "data.json"));
+const data = fs.readFileSync(require.join("data.json"));
 
 const thumbs = data.map((item) => {
   const obj = {
